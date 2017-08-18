6c551d15eab0601c19ad844f95f70af8b14a32c3
Nick DeJesus <ndejesus@sapient.com>
2017-08-18 01:25:12 -0400

    api clean up

--- src/components/util/api.js
+++ src/components/util/api.js
@@ -1,8 +1,5 @@
 import queryString from 'query-string';
 
-let key = {
-    data: 'a8457610b68381085a3fff38d6a36337: 6:74255139'
-}
 //get articles
 export const getArticles = () => {
     fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=a8457610b68381085a3fff38d6a36337:6:74255139`, {
@@ -12,7 +9,7 @@ export const getArticles = () => {
             'Content-Type': 'application/json',
         },
     }).then(response => {
-        console.log('get article response', response);
+
         return response.json();
     }).then(json => {
         console.log(json);
